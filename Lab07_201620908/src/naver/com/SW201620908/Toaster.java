62c0e0a9fdc79c8e8fb6b05adba7c861ee7ed945
Pyeon <lop1322@naver.com>
2017-05-01 11:19:49 +0900

    Code

--- Lab07_201620908/src/naver/com/SW201620908/Toaster.java
+++ Lab07_201620908/src/naver/com/SW201620908/Toaster.java
@@ -1,20 +1,5 @@
 package naver.com.SW201620908;
 
-public class Toaster extends Product implements Cooker {
-
-	@Override
-	public String prepareFood() {
-		
-		return "Let's eat Toasts!";
-	}
-
-	@Override
-	public String getName() {
-		
-		return getName();
-		
-	}
-	
-	
+public class Toaster {
 
 }
