fe91604e6fe2b6a6a1f382cd39621a031a8e5d0c
Pyeon <lop1322@naver.com>
2017-05-01 11:01:13 +0900

    토스터 클래스 생각점

---
+++ Lab07_201620908/src/naver/com/SW201620908/Product.java
@@ -0,0 +1,19 @@
+package naver.com.SW201620908;
+
+abstract public class Product {
+	
+	private double price;
+	
+	abstract public String getName();
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if(price!=0.0)
+		this.price = price;
+	}
+	
+	
+
+}
